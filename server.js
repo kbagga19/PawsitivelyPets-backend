bc8c5a571c4f7c41da2198c84f62a5d0a7129937
Karan Bagga <74136942+kbagga19@users.noreply.github.com>
2023-03-31 13:19:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -95,7 +95,8 @@ app.get("/profile", (req, res) => {
 });
 
 app.post("/logout", (req,res) => {
-  res.cookie('token', '').json('ok')
+  res.cookie('token', '').json('ok');
+  return false;
 });
 
 app.get("/food", async (req,res) => {
